5e7b42374025e8e53ad2a8604a2eb3b8139cbe29
Gilbert <gilbertbgarza@gmail.com>
2026-06-17 22:40:02 -0500

    Retro port acme

---
+++ bin/acme/dns/cloudflare.js
@@ -0,0 +1,38 @@
+export const timeout = 60000
+
+export const auth = {
+  zone: 'CF_ZONE',
+  token: 'CF_TOKEN'
+}
+
+export async function add(name, content, domain, { zone, token }) {
+  const x = await fetch('https://api.cloudflare.com/client/v4/zones/' + zone + '/dns_records', {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      content,
+      type: 'TXT',
+      comment: 'Cofound ACME challenge for ' + domain,
+      ttl: 120
+    })
+  })
+
+  if (!x.ok)
+    throw new Error('Bad cloudflare response ' + x.status + ': ' + (await x.text()))
+
+  return (await x.json()).result.id
+}
+
+export async function remove(id, { zone, token }) {
+  await fetch('https://api.cloudflare.com/client/v4/zones/' + zone + '/dns_records/' + id, {
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'X-Auth-Email': ''
+    }
+  })
+}
--- bin/help/usage.js
+++ bin/help/usage.js
@@ -12,6 +12,7 @@ cofound ${ c.bold`c` }${ c.dim`reate` }        Create a new cofound project
 cofound ${ c.bold`d` }${ c.dim`evelop` }       Starts the cofound development setup
 cofound ${ c.bold`g` }${ c.dim`enerate` }      Generate static HTML
 cofound ${ c.bold`b` }${ c.dim`uild` }         Build and bundle browser js
+cofound ${ c.bold`a` }${ c.dim`cme` }          Provision TLS certificates (Let's Encrypt/ACME)
 
 cofound ${ c.bold`v` }${ c.dim`ersion` }       Print the current versions
 cofound ${ c.bold`h` }${ c.dim`elp` }          Print the full help
